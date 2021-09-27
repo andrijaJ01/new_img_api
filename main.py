6add1d38c935daae3d89968c0347fbddd97f0d74
Andrija Jovanovic <46319817+andrijaJ01@users.noreply.github.com>
2021-09-27 12:37:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from fastapi import FastAPI, File, UploadFile, Response
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.param_functions import Form
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, UploadFile, Response
 from fastapi.responses import FileResponse
 from random import randint
 import string
 import random
 import os
 
+app = FastAPI()
+
 IMAGEDIR = "img/"
 
-app = FastAPI()
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<style>
+input {
+  border: 1px solid #242424;
+  border-radius:999px;
+  background-color: #f1f1f1;
+  padding: 10px;
+  font-size: 16px;
+}
+textarea:focus, input:focus{ border: none; }
+</style>
+<form action="/images/" enctype="multipart/form-data" method="post">
+<input name="file" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
+
 
 IDS=[]
 
@@ -19,53 +48,21 @@ for f in files:
 @app.post("/images/")
 async def create_upload_file(file: UploadFile = File(...)):
 
-    id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(5))
-    file.filename=f'{id}.jpg'
+    id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(9))
+    file.filename=f'{id}.png'
 
-    contents = await file.read()  # <-- Important!
+    contents = await file.read() 
     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
         f.write(contents)
     IDS.append(id)
-    return id
+    content =f"<head><title>SUCCESS</title></head><body>\n<p>To access image go to the<a href='/images/{id}'> this link</a></p></body>"
+    return HTMLResponse(content=content)
 
 @app.get("/images/{id}")
 async def read_img(id):
-    path = f"{IMAGEDIR}{id}.jpg"
+    path = f"{IMAGEDIR}{id}.png"
     return FileResponse(path)
 
 @app.get('/image/list')
 async def list_id():
     return IDS
-    
-"""
-I can't feel your hair, the river streaming down
-Out of your head and into my open lap.
-It once was such silk, always soft to feel,
-But now so sparse and wired.
-How frayed, such disarray.
-
-I can't feel your face, the mask so perfect
-Upon your head and kept so flawless.
-It once was porcelain, almost mystical,
-But now so torn and broken.
-How cracked, so out of place.
-
-I can't feel your hand, a thing so fragile
-That it might break if looked at wrong.
-It once was dainty, so delicate,
-But now so bent and shattered.
-How singed, so pulverized.
-
-I can't feel your touch, something I've loved
-Ever since we had once met long ago.
-It once was stimulation, exhilarating,
-But now so gone and so far away.
-How far could it possibly be?
-
-I can't feel your pulse, the thing I miss most.
-
-I can't feel.
-I can't.
-
-"""
-
